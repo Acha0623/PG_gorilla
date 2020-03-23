a6330b21dd6643715c6d6858948651e7c8e7054b
Feng <54845390+Acha0623@users.noreply.github.com>
2020-03-23 18:03:35 +0100

    Update do_LDblock.r

--- do_LDblock.r
+++ do_LDblock.r
@@ -1,14 +1,16 @@
 library(snpMatrix)
+
+#Gbb
 gbb <- read.plink("Gbb_block")
 ldgbb <- ld.snp(gbb, dep=791)
 plot.snp.dprime(ldgbb, filename="Gbb_LDblock.eps", res=100)
-
+#Gbg
 gbg <- read.plink("Gbg_block")
 ldgbg <- ld.snp(gbg, dep=1286)
 plot.snp.dprime(ldgbg, filename="Gbg_LDblock.eps", res=100)
-
+#Ggg
 ggg <- read.plink("Ggg_block")
 ldggg <- ld.snp(ggg, dep=2166)
-plot.snp.dprime(ldggg, filename="Ggg_LDblock.eps", res=00)
+plot.snp.dprime(ldggg, filename="Ggg_LDblock.eps", res=100)
 
 print("Done LDblock plotting")
