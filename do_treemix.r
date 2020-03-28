c1a2b51cdc8224a2b96d89618409afb2ad28f0bc
Feng <54845390+Acha0623@users.noreply.github.com>
2020-03-28 18:48:54 +0100

    Update do_treemix.r

--- do_treemix.r
+++ do_treemix.r
@@ -1,12 +1,14 @@
+#individual
 source("./treemix-1.13/src/plotting_funcs.R")
-png("tree_allsample21.png",width=700,height=900)
-plot_tree("tree_allsample21")
-png("res_allsample21.png",width=600,height=600)
-plot_resid("tree_allsample21", "indorder.txt")
+png("tree_allsample.png",width=1200,height=1600,res=100)
+plot_tree("tree_allsample")
+png("res_allsample.png",width=600,height=600,res=100)
+plot_resid("tree_allsample", "sample_order.txt")
+#popualtion
+source("./treemix-1.13/src/plotting_funcs.R")
+png("tree_allfamily.png",width=500,height=600,res=100)
+plot_tree("tree_allfamily")
+png("res_allfamily.png",width=400,height=400,res=100)
+plot_resid("tree_allfamily", "family_order.txt")
 
-
-png("tree_allfamily21.png",width=700,height=900)
-plot_tree("tree_allfamily21")
-png("res_allfamily21.png",width=600,height=600)
-plot_resid("tree_allfamily21", "poporder.txt")
 print("Done treemix")
