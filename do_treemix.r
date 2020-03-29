0a123a71a5c37e1bc7c53251e0e16130972e794a
Feng <54845390+Acha0623@users.noreply.github.com>
2020-03-30 00:05:11 +0200

    Update do_treemix.r

--- do_treemix.r
+++ do_treemix.r
@@ -6,7 +6,7 @@ png("res_allsample.png",width=600,height=600,res=100)
 plot_resid("tree_allsample", "sample_order.txt")
 #popualtion
 source("./treemix-1.13/src/plotting_funcs.R")
-png("tree_allfamily.png",width=500,height=600,res=100)
+png("tree_allfamily.png",width=600,height=600,res=100)
 plot_tree("tree_allfamily")
 png("res_allfamily.png",width=400,height=400,res=100)
 plot_resid("tree_allfamily", "family_order.txt")
